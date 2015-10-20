2da954fc9521e7cc5ef929c27a11752edcffcd26
Siarhei Babich <Siarhei.Babich@gmail.com>
2015-10-20 13:19:50 +0300

    Hello!

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello SB!" );
+        System.out.println( "Hello!" );
     }
     
     public static void doNothing() {
