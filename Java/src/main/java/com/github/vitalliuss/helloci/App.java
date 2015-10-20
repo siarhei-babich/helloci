a432e9535ee4c174523e7596b198eab4926ecda1
Siarhei Babich <Siarhei.Babich@gmail.com>
2015-10-20 12:11:21 +0300

    Hello CI!

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Siarhei!" );
+        System.out.println( "Hello CI!" );
     }
     
     public static void doNothing() {
