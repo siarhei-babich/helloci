5c530c6aa41d99d40886f9d353c07f4625ecbf6c
Siarhei Babich <Siarhei.Babich@gmail.com>
2015-10-20 14:09:21 +0300

    Hello Test!

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
+    	System.out.println( "Hello Test!" );
     	System.out.println( "Hello CI!" );
         System.out.println( "Hello SB!" );
     }
